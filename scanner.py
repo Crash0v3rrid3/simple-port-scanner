dca508fbdf441b3583a0c68db1a4619d8272904a
Crash0v3rrid3 <jain240298@gmail.com>
2019-07-20 17:34:13 +0530

    Removed naive argument parsing, added click, converted functions to class based processing, added threads for improved performance

--- scanner.py
+++ scanner.py
@@ -1,57 +1,190 @@
 import socket
-import sys
 import re
+import click
+import threading
+import time
 
-if len(sys.argv) < 4:
-	print('''Usage: {file} protocol ip port/s
-Examples:
-{file} tcp 192.168.1.11 80,100
-{file} udp 1.1.1.1 53
-{file} tcp www.google.com 50-100 '''.format(file=sys.argv[0]))	# Check if the arguments are correct
-	exit(1)
 
-protocol = sys.argv[1]
-ip = sys.argv[2]
-ports = sys.argv[3]
-match = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)	# Chech to see if IP Specified, Not the best and the most effective way
-if None == match:
-	try: 
-		ip = socket.gethostbyname(ip)	# Get the ip from the host name
-	except socket.gaierror: 			# Exception raised if hostname not reachable
-		print("There was an error resolving the host!\n Exiting Now...")	# Exit on exception
-		exit(1)
+# Type utilities
+class IpOrHostName(click.ParamType):
 
-if protocol == 'tcp':	# set the protocol
-	protocol = socket.SOCK_STREAM
-elif protocol == 'udp':
-	protocol = socket.SOCK_DGRAM
-else:
-	print("Invalid Protocol!\nExiting Now...")	# If not udp or tcp, exit
-	exit(1)
+	def convert(self, value, param, ctx):
+		"""Converts hostnames/domain names into IP's"""
+		# Is IP given?
+		match = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", value)
 
-try:
-	if('-' in ports):
-		ports = list(range(*list(map(int, ports.split('-')))))	# Format the ports based on -
-	elif(',' in ports):
-		ports = list(map(int, ports.split(',')))	# Format the ports based on ,
-except:
-	print('Invalid Ports Specified!')
-	exit(1)
+		if match is None:	# IP is not given
+			try:
+				value = socket.gethostbyname(value)  			# Get the ip from the host name
+			except socket.gaierror:  							# Exception raised if hostname not reachable
+				self.fail(
+					'There was an error resolving the host!',
+					param,
+					ctx
+				)
 
-if type(ports) != type([]):
-	ports = [int(ports)]
+		return value
 
-if len(list(filter(lambda x: x > 65535, ports))) > 0:	# Check if any invalid ports specified
-	print('Invalid Ports Specified!')
-	exit(1)
 
-for port in ports:
-	s = socket.socket(socket.AF_INET, protocol) # Create new socket
-	try:
-		s.connect((ip, port))		# connect to host on specified port
-	except ConnectionRefusedError:
-		print("{}:{} Connection refused".format(ip, port)) # if exception raised, host ip not reachable
-		s.close()
-		continue
-	print("{}:{} Port Open".format(ip, port))	# Else reachable
-	s.close()
+class PortRange(click.ParamType):
+
+	def convert(self, value, param, ctx):
+		"""Converts various port lists, ranges into list"""
+
+		values = []
+		if ',' in value: 										# custom list given
+			values = value.split(',')
+		else:
+			values = [value]
+
+		final_port_range = set()
+		for ports in values:									# Can be single port and range
+			if '-' in ports:
+				try:
+					final_port_range |= set(					# Extend the current range
+						range(
+							*list(
+								map(int, ports.split('-'))
+							)
+						)
+					)
+				except Exception as err:
+					self.fail(
+						'Invalid port range specified',
+						param,
+						ctx
+					)
+			else:												# Single port
+				final_port_range.add(int(ports))
+
+		return iter(tuple(final_port_range))
+
+
+# Scanner class
+class PortScanner:
+	class ScanResult:
+		OPEN = 0
+		FILTERED = 1
+		CLOSED = 2
+
+		def __init__(self, port, message, status):
+			self.port = port
+			self.message = message
+			self.status = status
+
+		def __str__(self):
+			return f'{self.port}\t{self.status}\t{self.message}'
+
+	def __init__(self, ip, port_range, protocol, thread_count=1, timeout_sleep=0.5):
+		self.scan_results = []
+
+		self.thread_count = thread_count
+		self.port_range = port_range
+		self.timeout_sleep = timeout_sleep
+		self.ip = ip
+		self.protocol = protocol
+		self.threads = []
+
+		self.scan_results_lock = threading.Lock()
+		self.port_range_lock = threading.Lock()
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.is_done():
+			raise StopIteration
+
+		while True:
+			while not self.scan_results:
+				time.sleep(self.timeout_sleep)
+			with self.scan_results_lock:
+				if self.scan_results:
+					return self.scan_results.pop(0)
+
+	def _start_scanner(self):
+		while True:
+			time.sleep(self.timeout_sleep)
+			try:
+				next_port = self.next_port()						# Next port to scan
+
+				s = socket.socket(socket.AF_INET, self.protocol)  	# Create new socket
+				try:
+					s.connect((self.ip, next_port))  				# connect to host on specified port
+					self.add_scan_result(
+						PortScanner.ScanResult(
+							next_port,
+							'Port is up!',
+							PortScanner.ScanResult.OPEN
+						)
+					)
+				except ConnectionRefusedError:
+					s.close()
+					self.add_scan_result(
+						PortScanner.ScanResult(
+							next_port,
+							'Port seems closed!',
+							PortScanner.ScanResult.CLOSED
+						)
+					)
+			except StopIteration as err:							# All ports scanned
+				break
+
+	def next_port(self):
+		return next(self.port_range)
+
+	def is_done(self):
+		with self.scan_results_lock:
+			if self.scan_results:
+				return False
+		for thread in self.threads:
+			if thread.is_alive():
+				return False
+		return True
+
+	def start_scanner(self):
+		for _ in range(self.thread_count):							# Start n threads for faster processing
+			thread = threading.Thread(
+				target=self._start_scanner,
+				daemon=True
+			)
+			thread.start()
+			self.threads.append(thread)
+
+	def add_scan_result(self, result):
+		with self.scan_results_lock:
+			self.scan_results.append(result)
+
+
+@click.command()
+@click.argument('ip', type=IpOrHostName())
+@click.option('-t', '--tcp', type=bool, default=True)
+@click.option('-u', '--udp', type=bool, default=False)
+@click.option('-T', '--thread-count', type=int, default=1)
+@click.option('-s', '--timeout', type=float, default=0.5)
+@click.argument('port_range', type=PortRange())
+def scan(ip, tcp, udp, port_range, thread_count, timeout):
+	if tcp and udp:
+		click.echo('Please specify a single protocol!')
+		exit(0)
+
+	protocol = socket.SOCK_STREAM if tcp else socket.SOCK_DGRAM
+	scanner = PortScanner(
+		ip,
+		port_range,
+		protocol,
+		thread_count=thread_count,
+		timeout_sleep=timeout
+	)
+	scanner.start_scanner()
+	result_itr = iter(scanner)
+	while True:
+		try:
+			next_result = next(result_itr)
+			print(next_result)
+		except StopIteration:
+			break
+
+
+if __name__ == '__main__':
+	scan()
