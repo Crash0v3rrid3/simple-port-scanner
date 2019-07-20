0bfd7e35f0c295644dd1f600b154277f284177d7
Crash0v3rrid3 <jain240298@gmail.com>
2019-07-20 18:32:21 +0530

    Removed ide files, separated click type utilities

---
+++ type_utilities.py
@@ -0,0 +1,57 @@
+import click
+import re
+import socket
+
+
+class IpOrHostName(click.ParamType):
+
+    def convert(self, value, param, ctx):
+        """Converts hostnames/domain names into IP's"""
+        # Is IP given?
+        match = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", value)
+
+        if match is None:	                                    # IP is not given
+            try:
+                value = socket.gethostbyname(value)  			# Get the ip from the host name
+            except socket.gaierror:  							# Exception raised if hostname not reachable
+                self.fail(
+                    'There was an error resolving the host!',
+                    param,
+                    ctx
+                )
+
+        return value
+
+
+class PortRange(click.ParamType):
+
+    def convert(self, value, param, ctx):
+        """Converts various port lists, ranges into list"""
+
+        values = []
+        if ',' in value: 										# custom list given
+            values = value.split(',')
+        else:
+            values = [value]
+
+        final_port_range = set()
+        for ports in values:									# Can be single port and range
+            if '-' in ports:
+                try:
+                    final_port_range |= set(					# Extend the current range
+                        range(
+                            *list(
+                                map(int, ports.split('-'))
+                            )
+                        )
+                    )
+                except Exception as err:
+                    self.fail(
+                        'Invalid port range specified',
+                        param,
+                        ctx
+                    )
+            else:												# Single port
+                final_port_range.add(int(ports))
+
+        return iter(tuple(final_port_range))
